badee7cd3c0d11ec2a1d120e0b4957ded23c989e
aadarsh010c <adarsh010c@gmail.com>
2024-03-25 21:57:59 +0530

    Added files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import CssBaseline from '@mui/material/CssBaseline';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <CssBaseline enableColorScheme/>
+      <h1 className='text'>
+        Hello world!!
+      </h1>
+    </>
+  )
+}
+
+export default App
