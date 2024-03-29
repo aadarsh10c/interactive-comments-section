efaae84501105acfe82862f62f89eaa97c696870
aadarsh010c <adarsh010c@gmail.com>
2024-03-29 22:47:20 +0530

    set the stack

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,14 @@
 import  Container  from '@mui/material/Container';
+import Stack from '@mui/material/Stack';
 
 export default function Comment(){
     return(
         <>
           <Container maxWidth={false} className='container' >
-            <p>This is the comment</p>
+            <Stack spacing={1.5}>
+              <p>This is comment 1</p>
+              <p>This is comment 2</p>
+            </Stack>
           </Container>
         </>
     )
